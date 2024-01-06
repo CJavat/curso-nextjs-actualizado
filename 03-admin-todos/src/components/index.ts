d3433f25dce32e064f23d1b1561d38f4af94b9a7
CJavatProyectos <121978673+CJavatProyectos@users.noreply.github.com>
2024-01-06 11:19:53 -0600

    SECCIÓN 12 / CAP 158 - EN CURSO

--- 03-admin-todos/src/components/index.ts
+++ 03-admin-todos/src/components/index.ts
@@ -1,5 +1,5 @@
+export { TabBar } from "./TabBar";
 export { Sidebar } from "./Sidebar";
 export { SidebarItem } from "./SidebarItem";
 export { TopMenu } from "./TopMenu";
 export { WidgetItem } from "./WidgetItem";
-
