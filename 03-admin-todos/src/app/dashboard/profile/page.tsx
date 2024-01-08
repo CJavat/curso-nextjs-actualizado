95ca005b2d8c9cb84311c3d7100fe9b5307fe464
CJavatProyectos <121978673+CJavatProyectos@users.noreply.github.com>
2024-01-07 21:47:17 -0600

    SECCIÓN 13 / CAP 179 - EN CURSO

---
+++ 03-admin-todos/src/app/dashboard/profile/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function ProfilePage() {
+  const { data:session } = useSession();
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <hr />
+
+      <div className="flex flex-col">
+        <span>{ session?.user?.name ?? 'No Name' }</span>
+        <span>{ session?.user?.name ?? 'No Email' }</span>
+        <span>{ session?.user?.image ?? 'No Image' }</span>
+      </div>
+    </div>
+  );
+}
--- 03-admin-todos/src/components/sidebar/Sidebar.tsx
+++ 03-admin-todos/src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-import { CiBookmarkCheck, CiLogout } from "react-icons/ci";
-import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from "react-icons/io5";
-import { SidebarItem } from ".";
+import { CiLogout } from "react-icons/ci";
+import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline, IoPerson, IoPersonOutline } from "react-icons/io5";
+import { LogOutButton, SidebarItem } from "..";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const menuItem = [
   {
@@ -30,10 +32,23 @@ const menuItem = [
     title: "Products",
     path: "/dashboard/products"
   },
+  {
+    icon: <IoPersonOutline />,
+    title: "Perfil",
+    path: "/dashboard/profile"
+  },
 ]
 
-export const Sidebar = () => {
-  const name = "hola";
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+  
+  const avatarURL = session?.user?.image 
+    ? session?.user?.image 
+    : 'https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp';
+
+  const userName = session?.user?.name ? session?.user?.name : "No Name";
+  //TODO: const userRole = session?.user?.name
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -53,12 +68,12 @@ export const Sidebar = () => {
           <Image
             width={ 128 }
             height={ 128 }
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src={ avatarURL}
             alt="ailus logo"
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            { userName }
           </h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
@@ -73,10 +88,7 @@ export const Sidebar = () => {
       </div>
 
       <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-          <CiLogout />
-          <span className="group-hover:text-gray-700">Logout</span>
-        </button>
+        <LogOutButton />
       </div>
     </aside>
   );
