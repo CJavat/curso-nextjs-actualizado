b58b46b0916e25df1d88b309e3d0ec85daf11bc9
CJavatProyectos <121978673+CJavatProyectos@users.noreply.github.com>
2024-01-12 07:07:48 -0600

    SECCIÓN 16 / CAP 237 - EN CURSO

---
+++ 04-teslo-shop/src/seed/seed-database.ts
@@ -0,0 +1,30 @@
+import prisma from "../lib/prisma";
+import { initialData } from "./seed";
+
+async function main() {
+
+  // 1. Borrar registros previos
+  await Promise.all([
+    prisma.productImage.deleteMany(),
+    prisma.product.deleteMany(),
+    prisma.category.deleteMany()
+  ])
+
+  const { categories, products } = initialData;
+
+  // Categorias
+  const categoriesData = categories.map( (name ) => ({ name }));
+  await prisma.category.createMany({
+    data: categoriesData
+  });
+
+
+
+  console.log("Seed Ejecutado Correctamente");
+}
+
+(() => {
+  if (process.env.NODE_ENV === "production") return;
+
+  main();
+})();
